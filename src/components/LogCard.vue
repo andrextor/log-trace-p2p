20c22b28e823171b0f01e745d8a3b90b59d8432a
Iván Andrés López G. <26684203+andrextor@users.noreply.github.com>
2026-02-04 23:53:41 -0500

    wip

--- src/components/LogCard.vue
+++ src/components/LogCard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import type { LogEvent } from '../logic/types';
-import { CATEGORY_STYLES } from '../logic/mappers/checkout/CheckoutConfig';
+import { CATEGORY_STYLES } from '../logic/mappers/checkout/checkoutConfigMap';
 
 const props = defineProps<{
   log: LogEvent;
